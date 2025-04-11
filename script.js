8aebb487a183eef2185e39c09c43cf0bcaca62d1
iren42 <isabelleren618@gmail.com>
2025-04-11 20:50:57 +0200

    refactor: InputValidator class for better organization

--- script.js
+++ script.js
@@ -1,77 +1,70 @@
-const email = document.querySelector("#email");
-const emailError = document.querySelector("#email + .errorMsg");
-
 // Regular expression for email validation as per HTML specification
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const NAME_REGEX = /^(?!-)[a-zA-Z -]*[a-zA-Z]$/;
 
-if (!email || !emailError)
-	throw new Error("No email input");
-email.addEventListener("input", () => {
-	if (email.validity.valueMissing)
-		emailError.textContent = "No value typed";
-	else if (!EMAIL_REGEX.test(email.value))
-		emailError.textContent = "Pattern does not match an email address";
-	else if (email.validity.typeMismatch) {
-		emailError.textContent = "Should look like 'an@example.com'";
-	} else {
-		emailError.textContent = "";
-	}
-});
-
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email + .errorMsg");
 const firstname = document.querySelector("#firstname");
 const firstnameError = document.querySelector("#firstname + .errorMsg");
 const lastname = document.querySelector("#lastname");
 const lastnameError = document.querySelector("#lastname + .errorMsg");
+const form = document.querySelector("#sign-up");
 
-const NAME_REGEX = /^(?!-)[a-zA-Z -]*[a-zA-Z]$/;
-
+if (!email || !emailError)
+	throw new Error("No email input");
 if (!firstname || !firstnameError)
 	throw new Error("No firstname input");
 if (!lastname || !lastnameError)
 	throw new Error("No lastname input");
+if (!form)
+	throw new Error("No form");
 
-firstname.addEventListener("input",
-	() => { handleInput(firstname, firstnameError, updateNameError); });
-
-lastname.addEventListener("input",
-	() => { handleInput(lastname, lastnameError, updateNameError); });
-
-function setClass(input, isValid) {
-	input.className = isValid ? "valid" : "invalid";
-};
-
-// Update error message and visibility
-function updateNameError(error, isValidInput) {
-	if (isValidInput) {
-		error.textContent = "";
-		error.className = "errorMsg";
-	} else {
-		error.textContent = "This is not a valid name";
-		error.className = "errorMsg show";
-	}
-};
-
-// Handle input event to update email validity
-function handleInput(input, error, fn) {
-	const isValid = isValidInput(input);
-	setClass(input, isValid);
-	fn(error, isValid);
-};
-
-function isValidInput(name) {
-	if (!name.validity.valueMissing) {
-		return (true);
-	}
-	if (!NAME_REGEX.test(name.value)) {
-		return (false);
-	}
-	if (name.validity.typeMismatch) {
-		return (false);
-	}
-	return (true);
+function InputValidator(input, error, callbackFn) {
+	this.input = input;
+	this.error = error;
+	this.callbackFn = callbackFn;
 }
 
-const form = document.querySelector("#sign-up");
+InputValidator.prototype.test = function() {
+	this.callbackFn(this.input, this.error);
+}
+
+const emailValidator = new InputValidator(email, emailError, updateEmailError);
+const firstnameValidator = new InputValidator(firstname, firstnameError, updateNameError);
+const lastnameValidator = new InputValidator(lastname, lastnameError, updateNameError);
+
+// Update error message and visibility
+function updateEmailError(email, error) {
+	error.className = "errorMsg show";
+	if (email.validity.valueMissing)
+		error.textContent = "No value typed";
+	else if (!EMAIL_REGEX.test(email.value))
+		error.textContent = "Pattern does not match an email address";
+	else if (email.validity.typeMismatch) {
+		error.textContent = "Should look like 'an@example.com'";
+	} else {
+		error.textContent = "";
+		error.className = "errorMsg";
+	}
+}
+
+function updateNameError(name, error) {
+	error.className = "errorMsg show";
+	if (!NAME_REGEX.test(name.value)) {
+		error.textContent = "This is not a valid name";
+	}
+	else if (name.validity.typeMismatch) {
+		error.textContent = "This is not the right type";
+	} else {
+		error.textContent = "";
+		error.className = "errorMsg";
+	}
+}
+
+email.addEventListener("input", () => { emailValidator.test(); });
+firstname.addEventListener("input", () => { firstnameValidator.test(); });
+lastname.addEventListener("input", () => { lastnameValidator.test(); });
+
 form.addEventListener("submit", event => {
 	event.preventDefault();
 	console.log("submit!");
