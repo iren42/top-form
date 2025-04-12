4263550d2692ad356cedb569ba0db1f3717c6689
iren42 <isabelleren618@gmail.com>
2025-04-12 09:51:00 +0200

    feat: postal code validation

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ const firstname = document.querySelector("#firstname");
 const firstnameError = document.querySelector("#firstname + .errorMsg");
 const lastname = document.querySelector("#lastname");
 const lastnameError = document.querySelector("#lastname + .errorMsg");
+const postalCode = document.querySelector("#postal-code");
+const postalCodeError = document.querySelector("#postal-code + .errorMsg");
 const form = document.querySelector("#sign-up");
 
+if (!postalCode || !postalCodeError)
+	throw new Error("No postal-code input");
 if (!email || !emailError)
 	throw new Error("No email input");
 if (!firstname || !firstnameError)
@@ -30,6 +34,7 @@ InputValidator.prototype.test = function() {
 }
 
 const emailValidator = new InputValidator(email, emailError, updateEmailError);
+const postalCodeValidator = new InputValidator(postalCode, postalCodeError, updatePostalCodeError);
 const firstnameValidator = new InputValidator(firstname, firstnameError, updateNameError);
 const lastnameValidator = new InputValidator(lastname, lastnameError, updateNameError);
 
@@ -61,7 +66,46 @@ function updateNameError(name, error) {
 	}
 }
 
+function updatePostalCodeError(postalCodeField, error) {
+	// For each country, defines the pattern that the postal code has to follow
+	const constraints = {
+		ch: [
+			"^(CH-)?\\d{4}$",
+			"Swiss postal codes must have exactly 4 digits: e.g. CH-1950 or 1950",
+		],
+		fr: [
+			"^(F-)?\\d{5}$",
+			"French postal codes must have exactly 5 digits: e.g. F-75012 or 75012",
+		],
+		de: [
+			"^(D-)?\\d{5}$",
+			"German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
+		],
+		nl: [
+			"^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+			"Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+		],
+		ot: ["", "",],
+	};
+
+	// Read the country id
+	const country = document.getElementById("country").value;
+
+	// Build the constraint checker
+	const constraint = new RegExp(constraints[country][0], "");
+
+	// Check it!
+	if (constraint.test(postalCodeField.value)) {
+		error.className = "errorMsg";
+		error.textContent = "";
+	} else {
+		error.className = "errorMsg show";
+		error.textContent = constraints[country][1];
+	}
+}
+
 email.addEventListener("input", () => { emailValidator.test(); });
+postalCode.addEventListener("input", () => { postalCodeValidator.test(); });
 firstname.addEventListener("input", () => { firstnameValidator.test(); });
 lastname.addEventListener("input", () => { lastnameValidator.test(); });
 
